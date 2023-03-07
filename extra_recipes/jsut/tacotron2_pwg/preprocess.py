13b1d2fa5f0e8e28d9251a110dd510bb47bd13f3
Ryuichi Yamamoto <zryuichi@gmail.com>
2023-03-07 17:54:40 +0900

    Fix librosa warings
    ref #45

--- extra_recipes/jsut/tacotron2_pwg/preprocess.py
+++ extra_recipes/jsut/tacotron2_pwg/preprocess.py
@@ -44,7 +44,7 @@ def preprocess(
     _sr, x = wavfile.read(wav_file)
     if x.dtype in [np.int16, np.int32]:
         x = (x / np.iinfo(x.dtype).max).astype(np.float64)
-    x = librosa.resample(x, _sr, sr)
+    x = librosa.resample(x, orig_sr=_sr, target_sr=sr)
     # workaround for over resampling: add a small white noise
     if sr > _sr:
         x = x + np.random.randn(len(x)) * (1 / 2 ** 15)
